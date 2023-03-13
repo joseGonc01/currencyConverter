96d90f34ddd48475687cd1059f235b24c5984369
FerB99 <jklisma25@gmail.com>
2023-03-13 09:33:32 -0400

    Vesão fucionanado

--- src/app/conversor.service.ts
+++ src/app/conversor.service.ts
@@ -1,6 +1,12 @@
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+}
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +16,8 @@ export class ConversorService {
   constructor(private http: HttpClient) { }
 
   RealizarConversao(): Observable<any>{
-    const apiKey = "xHOGCIdVPklZxNrOSV617tIqnAffZOKfnpSh"
-    const url = `https://currencyapi.net/api/v1/rates?key=${apiKey}`
-
+    const apiKey = "xHOGCIdVPklZxNrOSV617tIqnAffZOKfnpSh";
+    const url = `https://currencyapi.net/api/v1/rates?key=${apiKey}&base=USD`;
     return this.http.get<any>(url);
   }
 }
